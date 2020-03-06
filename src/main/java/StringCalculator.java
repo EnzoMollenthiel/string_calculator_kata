20988667505a8696fe9b16e614ce37496c9460c5
enzo <mollenthielenzo@gmail.com>
2020-03-06 14:24:09 +0100

    refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,18 +2,20 @@ public class StringCalculator {
 
     private static final String TO_MUCH_ARGUMENTS = "To much arguments";
 
-    public String add(String input) {
+    public String add(final String input) {
+
+        long count = input.chars().filter(ch -> ch == ',').count();
 
         if (input.length() == 0) return "0";
+        else if(count > 2) return TO_MUCH_ARGUMENTS;
 
         String[] numbers = input.split(",");
         Integer result = 0;
 
-        if(numbers.length > 3) return TO_MUCH_ARGUMENTS;
-
         for(String number: numbers) {
             result += Integer.parseInt(number);
         }
+
         return result.toString();
     }
 }
