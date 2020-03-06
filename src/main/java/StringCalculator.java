c107d873358c2db183cd6cb4af577b6ec46831f9
enzo <mollenthielenzo@gmail.com>
2020-03-06 12:58:33 +0100

    green

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,6 +4,8 @@ public class StringCalculator {
         String[] numbers = input.split(",");
         Integer result = 0;
 
+        if(numbers.length > 3) return "To much arguments";
+
         for(String number: numbers) {
             result += Integer.parseInt(number);
         }
