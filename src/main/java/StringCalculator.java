d665a9929d40bbfdc68a24c903793b6ced48b669
enzo <mollenthielenzo@gmail.com>
2020-03-06 15:28:52 +0100

    refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,10 +5,12 @@ public class StringCalculator {
     public String add(final String input) {
         int unexpectedCharPosition = input.indexOf(",\n");
 
-        if (input.length() == 0) return "0";
+        if (input.length() == 0)
+            return "0";
         else if (unexpectedCharPosition != -1)
             return "Number expected but '\\n' found at position " + (unexpectedCharPosition + 1) + ".";
-        else if (input.charAt(input.length() - 1) == ',') return "Number expected but EOF found.";
+        else if (input.charAt(input.length() - 1) == ',')
+            return "Number expected but EOF found.";
 
         String numbersString = input.replaceAll("\n",",");
         String[] numbers = numbersString.split(",");
