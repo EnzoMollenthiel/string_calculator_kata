84ed217485cd0b902630189e512a3ca1477a4141
enzo <mollenthielenzo@gmail.com>
2020-03-06 15:21:05 +0100

    refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,17 +3,15 @@ import java.math.BigDecimal;
 public class StringCalculator {
 
     public String add(final String input) {
-
-        if (input.length() == 0) return "0";
-
         int unexpectedCharPosition = input.indexOf(",\n");
 
-        if (unexpectedCharPosition != -1)
+        if (input.length() == 0) return "0";
+        else if (unexpectedCharPosition != -1)
             return "Number expected but '\\n' found at position " + (unexpectedCharPosition + 1) + ".";
 
         String numbersString = input.replaceAll("\n",",");
-
         String[] numbers = numbersString.split(",");
+
         BigDecimal result = new BigDecimal(0);
 
         for(String number: numbers) {
