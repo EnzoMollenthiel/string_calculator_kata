072e5ae54d1d1d0508f38dfcab6bc7242c3318b3
enzo <mollenthielenzo@gmail.com>
2020-03-06 13:00:33 +0100

    refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,12 @@
 public class StringCalculator {
 
+    private static final String TO_MUCH_ARGUMENTS = "To much arguments";
+
     public String add(String input) {
         String[] numbers = input.split(",");
         Integer result = 0;
 
-        if(numbers.length > 3) return "To much arguments";
+        if(numbers.length > 3) return TO_MUCH_ARGUMENTS;
 
         for(String number: numbers) {
             result += Integer.parseInt(number);
