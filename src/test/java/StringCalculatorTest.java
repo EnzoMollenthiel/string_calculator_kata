6392ae9d5f4e499932cb48642952554fc3949fa6
enzo <mollenthielenzo@gmail.com>
2020-03-06 12:52:09 +0100

    red

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,4 +11,11 @@ public class StringCalculatorTest {
 
         assertEquals("Should return the string: 3", "3", stringCalculator.add(imput));
     }
+
+    @Test
+    public void add_should_be_able_to_return_the_sum_of_3_arguments() {
+        String imput = "1,2,3";
+
+        assertEquals("Should return the string: 6", "6", stringCalculator.add(imput));
+    }
 }
